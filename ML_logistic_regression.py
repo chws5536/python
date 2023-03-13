7a333d8052d11b58506dd842e17e58b1bba3c0a3
Jun Ho Lee <junlee5536@gmail.com>
2023-03-13 11:47:50 +0900

    Updated sigmoid function

--- ML_logistic_regression.py
+++ ML_logistic_regression.py
@@ -6,6 +6,7 @@ x = np.array([[1., 2.],
               [4., 3.],
               [5., 3.],
               [6., 2.]])
+
 y = np.array([[0.], [0.], [0.], [1.], [1.], [1.]])
 
 weight = np.array([[0.],
@@ -17,9 +18,9 @@ learning_rate = 1e-2
 n = x.shape[0]
 k = x.shape[1]
 
-for i in range(1001):
+for i in range(1002):
     # sigmoid function
-    hypothesis = 1 / (1 + np.exp((x @ weight) + bias))
+    hypothesis = 1 / (1 + np.exp(-(x @ weight + bias)))
     # print(hypothesis)
 
     # cost = -y * np.log(hypothesis) - (1 - y) * np.log(1 - hypothesis)   #(6,1)
@@ -29,32 +30,32 @@ for i in range(1001):
     # calculate the derivative of the cost function wrt weight
     weight_gradient_list = np.zeros((2, 1))
     for j in range(k):
-        weight_gradient = np.diagonal(np.multiply(x[:, j], (y * np.exp((x @ weight) + bias) + y - 1)) / (np.exp((x @ weight) + bias) + 1))
+        weight_gradient = np.diagonal((-x[:, j] * ((y-1) * np.exp(x @ weight + bias) + y)) / (np.exp(x @ weight + bias) + 1))
+        # print(weight_gradient)
+        
         weight_mean = sum(weight_gradient) / n
         weight_gradient_list[j, :] = weight_mean
         # print(weight_gradient_list)
 
     # calculate the derivative of the cost function wrt bias
-    bias_gradient = (y * np.exp((x @ weight) + bias) + y - 1) / (1 + np.exp((x @ weight) + bias))
-    bias_mean = sum(bias_gradient)/ n
+    bias_gradient = -(np.exp(x @ weight + bias) * y - np.exp(x @ weight + bias) + y) / (np.exp(x @ weight + bias) + 1)
+    bias_mean = sum(bias_gradient) / n
     # print(bias_mean)
-    #
+    
     # update weight and bias, gradient descent
     # print(weight, bias)
     weight -= learning_rate * weight_gradient_list
     bias -= learning_rate * bias_mean
     # print(weight, bias)
 
-    if i % 100 == 0:
+    if i % 100 == 1:
         print(i, cost)
+    # print(i, cost)
 
 # test accuracy of weight and bias
 x_test = np.array([[5., 2.]])
 y_test = np.array([[1.]])
 n = x_test.shape[0]
 
-hypothesis = 1 / (1 + np.exp((x_test @ weight) + bias))
+hypothesis = 1 / (1 + np.exp(-(x_test @ weight + bias)))
 print(sum((hypothesis > 0.5) == y_test) / n)
-
-
-
