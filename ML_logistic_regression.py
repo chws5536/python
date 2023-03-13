a4d1fb92261f0b676f902316a14fff0d3a697e21
Jun Ho Lee <junlee5536@gmail.com>
2023-03-13 11:51:25 +0900

    Update ML_logistic_regression.py

--- ML_logistic_regression.py
+++ ML_logistic_regression.py
@@ -12,7 +12,6 @@ y = np.array([[0.], [0.], [0.], [1.], [1.], [1.]])
 weight = np.array([[0.],
                   [0.]])
 bias = np.array([0.])
-# print(x.shape, weight.shape, bias.shape)
 
 learning_rate = 1e-2
 n = x.shape[0]
@@ -21,7 +20,6 @@ k = x.shape[1]
 for i in range(1002):
     # sigmoid function
     hypothesis = 1 / (1 + np.exp(-(x @ weight + bias)))
-    # print(hypothesis)
 
     # cost = -y * np.log(hypothesis) - (1 - y) * np.log(1 - hypothesis)   #(6,1)
     # mean of cost function
@@ -31,26 +29,19 @@ for i in range(1002):
     weight_gradient_list = np.zeros((2, 1))
     for j in range(k):
         weight_gradient = np.diagonal((-x[:, j] * ((y-1) * np.exp(x @ weight + bias) + y)) / (np.exp(x @ weight + bias) + 1))
-        # print(weight_gradient)
-        
         weight_mean = sum(weight_gradient) / n
         weight_gradient_list[j, :] = weight_mean
-        # print(weight_gradient_list)
 
     # calculate the derivative of the cost function wrt bias
     bias_gradient = -(np.exp(x @ weight + bias) * y - np.exp(x @ weight + bias) + y) / (np.exp(x @ weight + bias) + 1)
     bias_mean = sum(bias_gradient) / n
-    # print(bias_mean)
     
     # update weight and bias, gradient descent
-    # print(weight, bias)
     weight -= learning_rate * weight_gradient_list
     bias -= learning_rate * bias_mean
-    # print(weight, bias)
 
     if i % 100 == 1:
         print(i, cost)
-    # print(i, cost)
 
 # test accuracy of weight and bias
 x_test = np.array([[5., 2.]])
