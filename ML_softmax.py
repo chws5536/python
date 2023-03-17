2114823d420e501ad7e07499c558893226150725
Jun Ho Lee <junlee5536@gmail.com>
2023-03-17 15:20:14 +0900

    Added d_CE_loss_function wrt weight and bias

--- ML_softmax.py
+++ ML_softmax.py
@@ -8,8 +8,6 @@ x = np.array([[1, 2, 1, 1],
           [1, 2, 5, 6],
           [1, 6, 6, 6],
           [1, 7, 7, 7]])
-
-# one-hot-encoding
 y = np.array([[0, 0, 1],
           [0, 0, 1],
           [0, 0, 1],
@@ -29,31 +27,32 @@ bias = np.array([-1.3080608 , -0.13253094,  0.5513761 ])
 n = x.shape[0]
 k_x = x.shape[1]
 k_y = y.shape[1]
+lr = 1e-1
 
-epochs = 1001
-lr = 1e-2
-
-for i in range(epochs):
+for i in range(2001):
     hypothesis = x @ weight + bias
-    # print(hypothesis)
     vector = np.sum(np.exp(hypothesis), axis=1)
     softmax = np.exp(hypothesis) / vector[:, None]
-    # print(softmax)
     cost = sum(-np.sum(y * np.log(softmax), axis=1)) / n
-    # print(cost)
 
-    #calculate the derivative of the cross entropy loss function with respect to weight and bias
+    # calculate the derivative of the cross-entropy loss function with respect to weight and bias
+    weight_gradient = (x.T @ (softmax-y)) / n
+    bias_gradient = sum(softmax-y) / n
 
     # update weight and bias, gradient descent
-    weight -= learning_rate * weight_gradient_list
-    bias -= learning_rate * bias_mean
+    weight -= lr * weight_gradient
+    bias -= lr * bias_gradient
 
-    if i % 100 == 1:
-        print(i, cost)
-        
-#test
-# x_test, y_test, n
+    print(i, cost)
+
+
+# test
+x_test = np.array([[2,1,3,2]])
+y = [[0,0,1]]
+
 hypothesis = x_test @ weight + bias
 vector = np.sum(np.exp(hypothesis), axis=1)
 softmax = np.exp(hypothesis) / vector[:, None]
+print(y)
+print('predicted index: ' + str(np.argmax(softmax)))
 
