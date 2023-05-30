38ba907f06254fbc3ca0dbc1092234036f69bc00
Jun Ho Lee <junlee5536@gmail.com>
2023-05-31 01:07:10 +0900

    Update eigenvalues_and_determinants.py

--- eigenvalues_and_determinants.py
+++ eigenvalues_and_determinants.py
@@ -1,32 +1,34 @@
 import numpy as np
 
 
-def det2x2(input):
-    return input[0][0] * input[1][1] - input[1][0] * input[0][1]
+def det2x2(A):
+    return A[0][0] * A[1][1] - A[1][0] * A[0][1]
 
 
-def is_singular2x2(input):
-    det = det2x2(input)
+def is_singular2x2(A):
+    det = det2x2(A)
     ans = det == 0
     return det, ans
-  
 
-def det3x3(input):
-  return input[0][0] * input[1][1] * input[2][2] + input[0][1] * input[1][2] * input[2][0] + input[0][2] * input[1][0] * input[2][1] \
-        - input[0][2] * input[1][1] * input[2][0] - input[0][1] * input[1][0] * input[2][2] - input[0][0] * input[1][2] * input[2][1]
 
-  
-def is_singular3x3(input):
-    det = det3x3(input)  
+def det3x3(A):
+    return A[0][0] * A[1][1] * A[2][2] + A[0][1] * A[1][2] * A[2][0] + A[0][2] * A[1][
+        0] * A[2][1] \
+           - A[0][2] * A[1][1] * A[2][0] - A[0][1] * A[1][0] * A[2][2] - A[0][0] * A[1][
+               2] * A[2][1]
+
+
+def is_singular3x3(A):
+    det = det3x3(A)
     ans = det == 0
     return det, ans
-  
-  
-def det_4x4(input):
-    n = input.shape[0]
+
+
+def det_4x4(A):
+    n = A.shape[0]
     sum = 0
     for i in range(n):
-        output = ((-1) ** i) * input[0, i] * det3x3(np.delete(np.delete(input, 0, 0), i, 1))
+        output = ((-1) ** i) * A[0, i] * det3x3(np.delete(np.delete(A, 0, 0), i, 1))
         sum += output
     return sum
 
@@ -43,7 +45,7 @@ def det_nxn(A):
 
     elif n == 3:
         det = A[0][0] * A[1][1] * A[2][2] + A[0][1] * A[1][2] * A[2][0] + A[0][2] * A[1][0] * A[2][1] \
-          - A[0][2] * A[1][1] * A[2][0] - A[0][1] * A[1][0] * A[2][2] - A[0][0] * A[1][2] * A[2][1]
+              - A[0][2] * A[1][1] * A[2][0] - A[0][1] * A[1][0] * A[2][2] - A[0][0] * A[1][2] * A[2][1]
 
     elif n == 2:
         det = A[0][0] * A[1][1] - A[1][0] * A[0][1]
@@ -52,31 +54,32 @@ def det_nxn(A):
 
     return det
 
+
 def is_singular_nxn(A):
     det = det_nxn(A)
     ans = det == 0
     return det, ans
 
-  
-def eigenvalues2x2(input):
+
+def eigenvalues2x2(A):
     # calculate coefficients for a quadratic formula
     a = 1
-    b = -(input[0][0] + input[1][1])
-    c = (input[0][0] * input[1][1]) - (input[0][1] * input[1][0])
+    b = -(A[0][0] + A[1][1])
+    c = (A[0][0] * A[1][1]) - (A[0][1] * A[1][0])
 
     # calculate eigenvalues by solving the quadratic equation
     r1 = (-b + np.sqrt(b ** 2 - 4 * a * c)) / 2 * a
     r2 = (-b - np.sqrt(b ** 2 - 4 * a * c)) / 2 * a
     r = np.array([r1, r2])
     return r
-  
-  
+
+
 def cramers_rule3x3(A, b):
     n = A.shape[0]
     k = A.shape[1]
     ans = np.zeros((n, k))
 
-    #calcualte determinant
+    # calcualte determinant
     det = 1 / det3x3(A)
 
     # calculate co-factor matrix
@@ -84,8 +87,8 @@ def cramers_rule3x3(A, b):
         for j in range(k):
             ans[i, j] = ((-1) ** (i + j)) * det2x2(np.delete(np.delete(A, i, 0), j, 1))
     return (det * ans).T @ b
-    
-    
+
+
 def is_positive_definite(A):
     n = A.shape[0]
     det = det_nxn(A)
@@ -187,7 +190,7 @@ def is_negative_semidefinite(A):
             det = det_nxn(A_sub_sub)
             if semi_sign(n, det) == 0:
                 return 0
-            
+
     n = A.shape[0]
     for k in range(n - 1, -1, -1):
         if A[k][k] > 0:
